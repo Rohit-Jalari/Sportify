2827bbdf2fe1be3c7155d88e6efd34d2d33f8f71
Rohit Jalari <58342571+Rohit-Jalari@users.noreply.github.com>
2023-06-22 17:27:44 +0545

    Front-End pages only
    Initial

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<html
+lang="en"
+class="light-style layout-menu-fixed"
+dir="ltr"
+data-theme="theme-default"
+data-assets-path="../sneat/assets/"
+data-template="vertical-menu-template-free"
+>
+<head>
+	<?php include('head.php'); ?>
+</head>
+
+<body>
+	<!-- Layout wrapper -->
+	<div class="layout-wrapper layout-content-navbar">
+		<div class="layout-container">
+			<!-- Menu -->
+			<?php include('aside.php')?>
+
+			<div class="layout-page" style="height: 150vh">
+				<?php include('navbar.php')?>
+				<!-- Content wrapper -->
+				<div class="content-wrapper">
+					<div class="container-xxl flex-grow-1 container-p-y">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<?php include('script.php')?>
+</html>
---
+++ login.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<html
+lang="en"
+class="light-style layout-menu-fixed"
+dir="ltr"
+data-theme="theme-default"
+data-assets-path="../sneat/assets/"
+data-template="vertical-menu-template-free"
+>
+<head>
+  <?php include('head.php'); ?>
+  <!-- Page CSS -->
+  <!-- Page -->
+  <link rel="stylesheet" href="assets/vendor/css/pages/page-auth.css" />
+</head>
+
+<body>
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <a href="index.php" class="app-brand-link gap-2">
+                <span class="app-brand-logo">
+                  <a href="index.php" class="py-2">
+                    <span class="logo text-dark">Sportify</span>
+                  </a>
+                </span>
+              </a>
+            </div>
+            <!-- /Logo -->
+            <h4 class="mb-2">Welcome to <span class="fw-bold mark">Sportify !</span> 👋</h4>
+            <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+            <form id="formAuthentication" class="mb-3" action="index.php" method="POST">
+              <div class="mb-3">
+                <label for="email" class="form-label">Email or Username</label>
+                <input
+                type="text"
+                class="form-control"
+                id="email"
+                name="email-username"
+                placeholder="Enter your email or username"
+                autofocus
+                />
+              </div>
+              <div class="mb-3 form-password-toggle">
+                <div class="d-flex justify-content-between">
+                  <label class="form-label" for="password">Password</label>
+                  <a href="forgotPassword.php">
+                    <small>Forgot Password?</small>
+                  </a>
+                </div>
+                <div class="input-group input-group-merge">
+                  <input
+                  type="password"
+                  id="password"
+                  class="form-control"
+                  name="password"
+                  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                  aria-describedby="password"
+                  />
+                  <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                </div>
+              </div>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="remember-me" />
+                  <label class="form-check-label" for="remember-me"> Remember Me </label>
+                </div>
+              </div>
+              <div class="mb-3">
+                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+              </div>
+            </form>
+
+            <p class="text-center">
+              <span>New on our platform?</span>
+              <a href="register.php">
+                <span>Create an account</span>
+              </a>
+            </p>
+          </div>
+        </div>
+        <!-- /Register -->
+      </div>
+    </div>
+  </div>
+</body>
+<?php include('script.php')?>
+</html>
+
