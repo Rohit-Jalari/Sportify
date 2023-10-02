5bc423fb9701e33896eecf4b5839655b33f25eb0
Roshani421 <evaprincess59@gmail.com>
2023-10-02 10:50:42 +0545

    update

---
+++ includes/gameIndex.php
@@ -0,0 +1,39 @@
+<?php
+require('../config/session.php');
+?>
+<!DOCTYPE html>
+<html>
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
+	data-template="vertical-menu-template-free">
+
+<head>
+	<?php include('../includes/head.php'); ?>
+</head>
+
+<body>
+	<!-- Layout wrapper -->
+	<div class="layout-wrapper layout-content-navbar">
+		<div class="layout-container">
+			<!-- Menu -->
+			<?php include('../includes/gameAside.php') ?>
+
+			<div class="layout-page" style="height: 150vh">
+				<?php include('../includes/navbar.php') ?>
+				<!-- Content wrapper -->
+				<div class="content-wrapper">
+					<div class="container-xxl flex-grow-1 container-p-y">
+						<?php
+						// print_r($_SESSION['userRecord']);
+						// print_r($tournamentDetail);
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Overlay -->
+		<div class="layout-overlay layout-menu-toggle"></div>
+	</div>
+</body>
+<?php include('../includes/script.php') ?>
+
+</html>
